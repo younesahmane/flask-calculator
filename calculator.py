f23063a3f9a3e6eb5dfaed3392b06a8595797ec9
benaoudia <145931083+benaoudia@users.noreply.github.com>
2024-10-20 19:48:15 +0100

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,6 +1,20 @@
 from abc import ABC, abstractmethod
 
 class Calculator(ABC):
+    def __init__(self, numberA: float, numberB: float):
+        self.numberA = numberA
+        self.numberB = numberB
+    
     @abstractmethod
     def calculate(self) -> float:
         pass
+
+class Multiply(Calculator):
+    def calculate(self) -> float:
+        return self.numberA * self.numberB
+
+class Divide(Calculator):
+    def calculate(self) -> float:
+        if self.numberB == 0:
+            raise ValueError("Division by zero is not allowed")
+        return self.numberA / self.numberB
